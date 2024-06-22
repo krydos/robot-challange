9a77e21526081cb7bad8707dbca0e0ec1a017e59
Ruslan Bekenev <furyinbox@gmail.com>
2024-06-22 21:01:09 +1000

    Remove TODO list from index.js
    I kept it there since the beginning
    and it's time to get rid of it.

--- index.ts
+++ index.ts
@@ -6,26 +6,6 @@ import { ConsoleOutputHandler } from './src/output_handler';
 import { SimpleMoveValidator } from './src/validators/simple_move_validator';
 import { SimpleParser } from './src/parsers/simple_parser';
 
-
-// TODO show usage
-// TODO lint variable names
-// DONE negative board dimensions shouldn't be possible... I guess...
-// DONE think about changing how left & right command works
-// DONE NopCommand and PlaceCommand should not be treated specially in the runner
-// DONE get rid of some DRY in tests
-// DONE make config to configure the board
-// DONE remove mutable field from commands (I don't think I use it)
-// DONE place command has terrible arguments handler
-// DONE don't use strings for directions
-// DONE validate robot movements
-// DONE validate commands input
-// DONE commands should be case insensitive
-// DONE show unknown command error in interactive mode
-// DONE get rid of test/data folder
-// DONE convert to ES6 modules
-// DONE Mark commands as mutable or not (this should help with figuring out if should check if move is valid)
-
-
 // ignore binary and the script name
 const args = process.argv.slice(2);
 
